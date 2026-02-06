bdb9822dab9ec2ec639e3321f3cda8e886c950f9
javiervg567 <javiervg567@alumnos.alborfp.com>
2026-02-06 16:07:56 +0100

    Tercera función del tablero

--- tablero_hundir_la_flota.py
+++ tablero_hundir_la_flota.py
@@ -20,3 +20,19 @@ def mostrar_tableros_paralelos(tab_jugador, tab_disparos):
         fila_d = "  ".join(tab_disparos[i])
         print(f"{letra} | {fila_j}      {letra} | {fila_d}")
     print("\n   TU FLOTA (Izquierda)      |      TUS DISPAROS (Derecha)")
+def es_posicion_valida(tablero, fila, col, longitud, orientacion):
+    tam = len(tablero)
+    for i in range(longitud):
+        r, c = (fila + i, col) if orientacion == "V" else (fila, col + i)
+        
+        if not (0 <= r < tam and 0 <= c < tam):
+            return False
+        
+        # Regla: No tocar otros barcos (revisar 3x3 alrededor de cada celda)
+        for dr in range(-1, 2):
+            for dc in range(-1, 2):
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < tam and 0 <= nc < tam:
+                    if tablero[nr][nc] == SIMBOLO_BARCO:
+                        return False
+    return True
